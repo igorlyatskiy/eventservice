cbe8ddfd7f212ad7ccde20424bd351fbc6e60f0e
Igor Lyatskiy <igor.lyatskiy@gmail.com>
2021-03-07 20:53:37 +0300

    feat: make dynamic date

--- event-api-service/app.js
+++ event-api-service/app.js
@@ -1,23 +1,40 @@
-'use strict';
+"use strict";
 
-var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
+var SwaggerExpress = require("swagger-express-mw");
+var app = require("express")();
 module.exports = app; // for testing
 
 var config = {
-  appRoot: __dirname // required config
+	appRoot: __dirname, // required config
 };
 
-SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
+SwaggerExpress.create(config, function (err, swaggerExpress) {
+	if (err) {
+		throw err;
+	}
 
-  // install middleware
-  swaggerExpress.register(app);
+	// install middleware
+	swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
-  app.listen(port);
+	var port = process.env.PORT || 10010;
+	app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/events']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/api/v1/events?date=2021-02-22');
-  }
+	const dateRightNow = new Date();
+	let fullDateText = `${dateRightNow.getFullYear()}-${
+		dateRightNow.getMonth() + 1 > 10
+			? dateRightNow.getMonth() + 1
+			: "0" + (dateRightNow.getMonth() + 1)
+	}-${
+		dateRightNow.getDate() > 10
+			? dateRightNow.getDate()
+			: "0" + dateRightNow.getDate()
+	}`;
+
+	if (swaggerExpress.runner.swagger.paths["/events"]) {
+		console.log(
+			"try this:\ncurl http://127.0.0.1:" +
+				port +
+				`/api/v1/events?date=${fullDateText}`
+		);
+	}
 });
